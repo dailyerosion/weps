7929e1b3072d20261c5d2d39b711e8161da6ffad
Fred Fox <fred.fox@usda.gov>
2016-05-09 17:12:06 +0000

    Merged (manually) changes  from trunk checkin 13352, 13379, 13481
    hydrobal.out reporting updated to show water use efficiency when a crop is growing, and fallow efficiency only when no crop is growing.
    
    git-svn-id: https://infosys.ars.usda.gov/svn/code/weps1/branches/weps.src.subregion@13554 77af8a48-1994-4e4a-b6f4-b1c89642997b

--- src/lib_hydro/report_hydrobal.for
+++ src/lib_hydro/report_hydrobal.for
@@ -22,10 +22,13 @@
 
 !     + + + LOCAL VARIABLES + + +
       real fallow_eff
+      real water_use_eff
+      real water_use
 
 !     + + + LOCAL DEFINITIONS + + +
-!     fallow_eff - computed fallow efficiency from period rain and soil
-!                  water content values
+!     fallow_eff - computed fallow efficiency from period rain and soil water content values
+!     water_use_eff - computed water use efficiency from period rain and soil water content values
+!     water use - The total water that was used during period
 
       if( am0sif ) then  ! initilizing cycle
 
@@ -48,11 +51,37 @@
         end do
 
         if ( cumprecip(isr) .gt. 0.0 ) then
-            fallow_eff = (presswc(isr) - initswc(isr) - cumirrig(isr))  &
+            fallow_eff = ( presswc(isr) - initswc(isr)                  &
+     &                 + pressnow(isr) - initsnow(isr) - cumirrig(isr) )&
      &                 / cumprecip(isr)
         else
             fallow_eff = 0.0
         end if
+
+        water_use = ( initswc(isr) - presswc(isr)                       &
+     &            + pressnow(isr) - initsnow(isr)                       &
+     &            + cumprecip(isr) + cumirrig(isr) )
+        if( water_use .ne. 0.0 ) then
+            water_use_eff = cumtrans(isr) / water_use
+        else
+            water_use_eff = 0.0
+        end if
+
+        if( cumtrans(isr) .gt. 0.0 ) then
+
+        write(unit=luohydrobal(isr),fmt=1000, advance='NO')             &
+     &  lastoper(isr)%day, lastoper(isr)%mon, lastoper(isr)%yr,         &
+     &  trim(lastoper(isr)%name),                                       &
+     &  'Start day,swc,snow', initday(isr), initswc(isr), initsnow(isr),&
+     &  'End day,swc,snow', presday(isr), presswc(isr), pressnow(isr),  &
+     &  'rain,irrigation,runoff,evap,trans,drain,check,wateruse_eff',   &
+     &  cumprecip(isr), cumirrig(isr),                                  &
+     &  cumrunoff(isr), cumevap(isr), cumtrans(isr), cumdrain(isr),     &
+     &  initswc(isr) - presswc(isr) + initsnow(isr) - pressnow(isr)     &
+     &  + cumprecip(isr) + cumirrig(isr) - cumrunoff(isr) - cumevap(isr)&
+     &  - cumtrans(isr) - cumdrain(isr), water_use_eff
+
+        else
 
         write(unit=luohydrobal(isr),fmt=1000, advance='NO')             &
      &  lastoper(isr)%day, lastoper(isr)%mon, lastoper(isr)%yr,         &
@@ -65,6 +94,8 @@
      &  initswc(isr) - presswc(isr) + initsnow(isr) - pressnow(isr)     &
      &  + cumprecip(isr) + cumirrig(isr) - cumrunoff(isr) - cumevap(isr)&
      &  - cumtrans(isr) - cumdrain(isr), fallow_eff
+
+        end if
 
  1000 format(1x,i2,'/',i2,'/',i2,'|',a,'|',a,'|',f7.0,'|',2(f9.3,'|'),  &
      &       a,'|',f7.0,'|',2(f9.3,'|'),a,'|',8(f9.3,'|'))
