8405ee97696f2db483297dd30cab9b0ad78f6ca6
Fred Fox <fred.fox@usda.gov>
2019-07-23 22:53:45 +0000

    Added debug statement to build for windows ifort failures
    git-svn-id: https://infosys.ars.usda.gov/svn/code/weps1/branches/weps.src.subregion@17591 77af8a48-1994-4e4a-b6f4-b1c89642997b

--- jenkins/setup.cmd
+++ jenkins/setup.cmd
@@ -7,11 +7,11 @@ set compiler=%2
 set mode=%3
 
 if "%arch%"=="x86" (
-	echo Calling ia32
+	echo Calling ia32 windows
 	rem call "C:\Program Files (x86)\Intel\Composer XE\bin\compilervars.bat" ia32
 	call "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\bin\compilervars.bat" ia32
 ) else if "%arch%"=="x86_64" (
-	echo Calling intel64
+	echo Calling intel64 windows
 	rem call "C:\Program Files (x86)\Intel\Composer XE\bin\compilervars.bat" intel64
 	call "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\bin\compilervars.bat" intel64
 ) else (
